9726cad9206673be1ef38782ddefa0e7dafed924
Arash Askari <33096218+arascch@users.noreply.github.com>
2022-04-30 21:13:29 +0430

    fix

--- shop/views.py
+++ shop/views.py
@@ -15,5 +15,5 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProdictForm()
-    return render(request , 'shop/product/detail.html' , {'product':product})
+    return render(request , 'shop/product/detail.html' , {'product':product , 'cart_product_form':cart_product_form})
 
