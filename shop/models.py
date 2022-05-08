6b5ed9be22fbe60373bf834ee1dbdfea4820e2bd
Arash Askari <33096218+arascch@users.noreply.github.com>
2022-05-08 20:59:33 +0430

    add comment

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from tabnanny import verbose
 from django.db import models
 from django.urls import reverse
 
+#all models for shop project 
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
