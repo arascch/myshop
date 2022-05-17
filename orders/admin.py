c5ed923d9a9a93b9227ce4647c87b26c73de420e
Arash Askari <33096218+arascch@users.noreply.github.com>
2022-05-17 21:31:34 +0430

    add order admin into the admin file

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OrderItem, Order
 
-# Register your models here.
+
+class OrderItemline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
